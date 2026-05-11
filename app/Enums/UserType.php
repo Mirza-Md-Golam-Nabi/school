d28b1c1f5dbc7df47ef7eecc84e7c17d545ef8f4
Nabi <g.nabi@shadhinbd.com>
2026-05-11 12:04:45 +0600

    Added getLabel() and options() method in UserType Enum file

--- app/Enums/UserType.php
+++ app/Enums/UserType.php
@@ -9,4 +9,22 @@ enum UserType: string
     case Teacher = 'teacher';
     case Student = 'student';
     case Staff = 'staff';
+
+    public function getLabel(): string
+    {
+        return match ($this) {
+            self::SuperAdmin => 'Super Admin',
+            self::Admin => 'Admin',
+            self::Teacher => 'Teacher',
+            self::Student => 'Student',
+            self::Staff => 'Staff',
+        };
+    }
+
+    public static function options(): array
+    {
+        return collect(self::cases())
+            ->mapWithKeys(fn ($case) => [$case->value => $case->getLabel()])
+            ->toArray();
+    }
 }
